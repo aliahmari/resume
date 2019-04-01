e4a08fc0bd673bbed688280da744347c7cd86d3a
Ali Ahmari <aliahmari.sn@gmail.com>
2019-04-01 23:53:26 +0430

    Updates

---
+++ precache-manifest.270b526c6f848a46273450201208ac27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "cec24249bdeca9883915",
+    "url": "/resume/static/js/main.a57d1c0f.chunk.js"
+  },
+  {
+    "revision": "edf0dd151b1094dc75bc",
+    "url": "/resume/static/js/2.e040d2e1.chunk.js"
+  },
+  {
+    "revision": "cec24249bdeca9883915",
+    "url": "/resume/static/css/main.f4fb878f.chunk.css"
+  },
+  {
+    "revision": "ec8b7adddd0228fa80e3183408b1518f",
+    "url": "/resume/index.html"
+  }
+];
